86af5e2e7ba544b226eeb5b0b7df3f01a24e2396
huangyede <691715912@qq.com>
2017-09-21 16:04:32 +0800

    init commit

---
+++ src/main/java/com/cwsm/CwsmApplication.java
@@ -0,0 +1,12 @@
+package com.cwsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwsmApplication.class, args);
+	}
+}
