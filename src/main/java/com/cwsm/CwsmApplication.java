b22a5ee6034b9f94b2b5fa2147d1d3c2e6dbbd9c
hyede <691715912@qq.com>
2018-10-11 09:54:15 +0800

    改war包

--- src/main/java/com/cwsm/CwsmApplication.java
+++ src/main/java/com/cwsm/CwsmApplication.java
@@ -2,10 +2,16 @@ package com.cwsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CwsmApplication {
+public class CwsmApplication  extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CwsmApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CwsmApplication.class, args);
 	}
